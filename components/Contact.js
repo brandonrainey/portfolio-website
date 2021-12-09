6c412b956aa631c6f368c6a36b344fbf9131e668
Brandon Rainey <brandonhrainey@gmail.com>
2021-12-09 02:40:10 -0500

    added head component

--- components/Contact.js
+++ components/Contact.js
@@ -3,6 +3,10 @@ import React, { useRef, useEffect } from "react";
 export default function Contact(props) {
   const contactDiv = useRef();
 
+  const emailText = useRef();
+
+  
+
   useEffect(() => {
     if (props.contactScroll === true) {
       contactDiv.current.scrollIntoView({
@@ -14,13 +18,14 @@ export default function Contact(props) {
     }
   }, [props.contactScroll]);
 
+  console.log(emailText.current)
   return (
     <div
       ref={contactDiv}
       className="flex justify-center mt-40 flex-col text-center"
     >
       <header className="font-bold text-5xl">Contact</header>
-      <p className="font-bold underline mt-6">brandonhrainey@gmail.com</p>
+      <p className="font-bold underline mt-6" ref={emailText} value='brandonhrainey@gmail.com' onClick={() => {navigator.clipboard.writeText(emailText.current.value)}}>brandonhrainey@gmail.com</p>
       <div className="flex justify-center mt-4">
         <a href="https://github.com/Genlord">
           <img src="/githubIcon.png" className="h-10 w-10" />
--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Head from "next/head"
 
 export default function Header(props) {
   return (
     <div className="flex h-40">
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
       <h1 className="mt-auto ml-8 font-bold">My Portfolio</h1>
+
       <nav className="ml-auto mt-auto">
         <button onClick={() => props.setWorkScroll(true)} className="mr-4">
           Projects
