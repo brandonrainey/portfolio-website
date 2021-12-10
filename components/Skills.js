85cc0d380bd938dc100335933da8a8d83257bc26
Brandon Rainey <brandonhrainey@gmail.com>
2021-12-10 01:23:38 -0500

    added img alt tags

--- components/Skills.js
+++ components/Skills.js
@@ -6,12 +6,12 @@ export default function Skills() {
       <h1 className="font-bold text-5xl">Technologies</h1>
       <div className="flex justify-center">
         <div className="flex justify-center mt-6 gap-x-10 gap-y-6 skills flex-wrap">
-          <img src="/cssLogo.png" className="h-28 w-30" />
-          <img src="/htmlLogo.png" className="h-24 w-30 mt-2" />
-          <img src="/jsIcon.png" className="h-20 w-26 mt-6" />
-          <img src="/reactIcon.png" className="h-24 w-30 mt-4" />
-          <img src="/nextjsIcon.png" className="h-24 w-30" />
-          <img src="/tailwindIcon.png" className="h-20 w-28" />
+          <img src="/cssLogo.png" className="h-28 w-30" alt='css logo'/>
+          <img src="/htmlLogo.png" className="h-24 w-30 mt-2" alt='html logo'/>
+          <img src="/jsIcon.png" className="h-20 w-26 mt-6" alt='javascript logo'/>
+          <img src="/reactIcon.png" className="h-24 w-30 mt-4" alt='react logo'/>
+          <img src="/nextjsIcon.png" className="h-24 w-30" alt='next js logo'/>
+          <img src="/tailwindIcon.png" className="h-20 w-28" alt='tailwind css logo'/>
         </div>
       </div>
     </div>
