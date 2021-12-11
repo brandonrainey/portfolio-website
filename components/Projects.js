7f515b5da9fc48b8f21bb983265701fd2fa52ec1
Brandon Rainey <brandonhrainey@gmail.com>
2021-12-10 20:28:32 -0500

    reformated the hover effect on each project

--- components/Projects.js
+++ components/Projects.js
@@ -21,93 +21,32 @@ export default function Work(props) {
     >
       <header className="font-bold text-5xl mb-10">Projects</header>
 
-      <div className="flex justify-center mt-6 projectContainer relative">
-        <div className="flex flex-row justify-around justify-center">
-          {/*anime db overlay */}
-          <div className=" absolute h-60  overlayOne flex justify-center gap-x-10 animeDB align-center">
-            <div className="mt-28 opacity-100">
-              <a
-                href="https://mal-anime-db.netlify.app"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Live
-              </a>
-            </div>
-            <div className="mt-28">
-              <a
-                href="https://github.com/Genlord/anime-app"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Github
-              </a>
-            </div>
-          </div>
-
-          {/*e-commerce overlay */}
-          <div className="w-96 absolute h-60  overlayOne flex justify-center gap-x-10 eCommerce">
-            <div className="mt-28 opacity-100">
-              <a
-                href="https://compparts.netlify.app"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Live
-              </a>
-            </div>
-            <div className="mt-28">
-              <a
-                href="https://github.com/Genlord/e-commerce"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Github
-              </a>
-            </div>
-          </div>
-          
-          {/*nft tracker overlay */}
-          <div className="w-96 absolute h-60  overlayOne flex justify-center gap-x-10 nftTracker">
-            <div className="mt-28 opacity-100 ">
-              <a
-                href="https://my-nft-tracker.netlify.app"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Live
-              </a>
-            </div>
-            <div className="mt-28">
-              <a
-                href="https://github.com/Genlord/NFT-Tracker"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Github
-              </a>
-            </div>
-          </div>
-
-          {/*room-homepage overlay */}
-          <div className="w-96 absolute h-60  overlayOne flex justify-center gap-x-10 roomHomepage">
-            <div className="mt-28 opacity-100">
-              <a
-                href="https://the-room-homepage.netlify.app"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Live
-              </a>
-            </div>
-            <div className="mt-28">
-              <a
-                href="https://github.com/Genlord/room-homepage"
-                className="font-bold text-2xl underline hover:text-blue-400"
-              >
-                Github
-              </a>
-            </div>
-          </div>
-        </div>
+      <div className="flex justify-center mt-6 projectContainer">
+        <div className="flex flex-row justify-around justify-center absolute"></div>
         {/* projects */}
-        <div className="flex flex-row justify-center w-full justify-around projWrapper">
-          <div className="flex w-96 flex-col projectOneWrapper">
+        <div className="flex flex-row justify-center w-full justify-around projWrapper relative">
+          {/* anime DB */}
+          <div className="flex w-96 flex-col projectOneWrapper relative ">
+            <div className=" absolute h-60 flex justify-center gap-x-10 align-center projectOverlay rounded">
+              <div className="mt-28 ">
+                <a
+                  href="https://mal-anime-db.netlify.app"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Live
+                </a>
+              </div>
+              <div className="mt-28">
+                <a
+                  href="https://github.com/Genlord/anime-app"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Github
+                </a>
+              </div>
+            </div>
+
             <section className="font-bold projectTitle text-xl proj1tt w-full justify-self-center">
-              
               Anime DB
             </section>
 
@@ -116,10 +55,14 @@ export default function Work(props) {
                 className="w-full relative projectOne"
                 href="https://mal-anime-db.netlify.app"
               >
-                <img src="/animedbss.png" className="rounded h-60 w-full" alt='anime db screenshot'/>
+                <img
+                  src="/animedbss.png"
+                  className="rounded h-60 w-full"
+                  alt="anime db screenshot"
+                />
               </a>
             </div>
-            <div className='aboutProj flex justify-center'>
+            <div className="aboutProj flex justify-center">
               <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-gray-700 mt-10 -ml-30 text-sm w-96 descriptionOne">
                 Anime database built with next.js and tailwind css. Takes data
                 from MAL API and displays it.
@@ -128,9 +71,28 @@ export default function Work(props) {
             </div>
           </div>
 
-          <div className="flex flex-col w-96 projectTwoWrapper">
+          {/* e-commerce */}
+          <div className="flex flex-col w-96 projectTwoWrapper relative">
+            <div className="w-96 absolute h-60 flex justify-center gap-x-10 projectOverlay rounded">
+              <div className="mt-28 opacity-100">
+                <a
+                  href="https://compparts.netlify.app"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Live
+                </a>
+              </div>
+              <div className="mt-28">
+                <a
+                  href="https://github.com/Genlord/e-commerce"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Github
+                </a>
+              </div>
+            </div>
+
             <section className="font-bold projectTitle2 text-xl proj1tt w-full justify-self-center">
-              
               E-Commerce
             </section>
 
@@ -139,10 +101,14 @@ export default function Work(props) {
                 className="w-full projectTwo"
                 href="https://compparts.netlify.app"
               >
-                <img src="/e-commercess.png" className="rounded h-60" alt='e-commerse screenshot'/>
+                <img
+                  src="/e-commercess.png"
+                  className="rounded h-60"
+                  alt="e-commerse screenshot"
+                />
               </a>
             </div>
-            <div className='aboutProj flex justify-center'>
+            <div className="aboutProj flex justify-center">
               <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-gray-700 mt-10 -ml-30 text-sm w-96 descriptionTwo">
                 Basic e-commerce template site built with Create-React-App and
                 Material UI. Uses Firebase Authentication and Firestore Database
@@ -152,21 +118,44 @@ export default function Work(props) {
             </div>
           </div>
 
-          <div className="flex w-24 flex-col w-96 projectTwoWrapper">
+          {/* nft-tracker */}
+          <div className="flex w-24 flex-col w-96 projectThreeWrapper relative">
+            <div className="w-96 absolute h-60 flex justify-center gap-x-10 projectOverlay rounded">
+              <div className="mt-28 opacity-100 ">
+                <a
+                  href="https://my-nft-tracker.netlify.app"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Live
+                </a>
+              </div>
+              <div className="mt-28">
+                <a
+                  href="https://github.com/Genlord/NFT-Tracker"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Github
+                </a>
+              </div>
+            </div>
+
             <section className="font-bold projectTitle3 text-xl proj1tt w-full justify-self-center">
-              
               NFT-Tracker
             </section>
 
             <div className="flex justify-center">
               <a
-                className="w-full projectTwo"
+                className="w-full projectThree"
                 href="https://my-nft-tracker.netlify.app"
               >
-                <img src="/nfttrackerss.png" className="rounded h-60" alt='nft tracker screenshot'/>
+                <img
+                  src="/nfttrackerss.png"
+                  className="rounded h-60"
+                  alt="nft tracker screenshot"
+                />
               </a>
             </div>
-            <div className='aboutProj flex justify-center'>
+            <div className="aboutProj flex justify-center">
               <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-gray-700 mt-10 -ml-30 text-sm w-96 descriptionThree">
                 A NFT tracker website built with next.js and tailwind css. Users
                 can enter thier ETH wallet address and displays cards with their
@@ -176,9 +165,28 @@ export default function Work(props) {
             </div>
           </div>
 
-          <div className="flex flex-col w-96 projectFourWrapper">
+          {/* room-homepage */}
+          <div className="flex flex-col w-96 projectFourWrapper relative">
+            <div className="w-96 absolute h-60 flex justify-center gap-x-10 projectOverlay">
+              <div className="mt-28 opacity-100">
+                <a
+                  href="https://the-room-homepage.netlify.app"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Live
+                </a>
+              </div>
+              <div className="mt-28">
+                <a
+                  href="https://github.com/Genlord/room-homepage"
+                  className="font-bold text-2xl underline hover:text-blue-400"
+                >
+                  Github
+                </a>
+              </div>
+            </div>
+
             <section className="font-bold projectTitle4 text-xl proj1tt w-full justify-self-center">
-              
               Room-Homepage
             </section>
 
@@ -187,14 +195,19 @@ export default function Work(props) {
                 className="w-full projectTwo"
                 href="https://the-room-homepage.netlify.app"
               >
-                <img src="/roomHomepageSS.png" className="rounded h-60" alt='room homepage screenshot'/>
+                <img
+                  src="/roomHomepageSS.png"
+                  className="rounded h-60"
+                  alt="room homepage screenshot"
+                />
               </a>
             </div>
-            <div className='aboutProj flex justify-center'>
+            <div className="aboutProj flex justify-center">
               <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-gray-700 mt-10 -ml-30 text-sm w-96 descriptionTwo">
-                Static homepage made from a FrontendMentor challenge. Active states on the nav and buttons, and cycles through three hero images.
-                Created with nextjs 
-                and basic css grid and flexbox layout.
+                Static homepage made from a FrontendMentor challenge. Active
+                states on the nav and buttons, and cycles through three hero
+                images. Created with nextjs and basic css grid and flexbox
+                layout.
               </span>
               About
             </div>
