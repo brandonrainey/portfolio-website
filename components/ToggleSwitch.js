5ddbb4f75eb8be7a75620d1764913463d6988326
Brandon Rainey <brandonhrainey@gmail.com>
2022-01-29 00:23:37 -0500

    changed to htmlFor

--- components/ToggleSwitch.js
+++ components/ToggleSwitch.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { FaSun } from 'react-icons/fa';
-import { FaMoon } from 'react-icons/fa';
+import React from "react";
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
-export default function ToggleSwitch({ lightMode, setLightMode}) {
+export default function ToggleSwitch({ lightMode, setLightMode }) {
   const handleChange = () => {
     setLightMode(!lightMode);
   };
 
+  return (
+    <div className="toggleWrapper">
+      <input
+        type="checkbox"
+        id="switch"
+        className="toggleInput"
+        checked={lightMode}
+        onChange={handleChange}
+      />
 
-  return <div className='toggleWrapper'>
-            <input type="checkbox" id="switch" className='toggleInput' checked={lightMode} onChange={handleChange}/>
-            
-            <label for="switch" className='toggleLabel'><div className='iconWrapper'><FaSun className='sunIcon'/> <FaMoon className='moonIcon'/></div></label>
+      <label htmlFor="switch" className="toggleLabel">
+        <div className="iconWrapper">
+          <FaSun className="sunIcon" /> <FaMoon className="moonIcon" />
         </div>
-  
-  
+      </label>
+    </div>
+  );
 }
