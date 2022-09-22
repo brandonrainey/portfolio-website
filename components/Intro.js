9d749b8d1784399631089aae23ef7c45639765d1
Brandon Rainey <brandonhrainey@gmail.com>
2022-09-22 01:01:08 -0400

    some mobile intro changes

--- components/Intro.js
+++ components/Intro.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <div className="flex flex-col justify-center text-center mt-52 intro">
+    <div className="flex flex-col justify-center text-center mt-24 md:mt-52 intro">
       <h1 className="font-bold text-4xl tracking-wide">Hi, Im Brandon</h1>
       <div className="flex justify-center mt-4 introContainer">
-        <p className="text-6xl w-2/3 text-center font-bold introText tracking-wide">
+        <p className="md:text-6xl text-3xl w-3/4 text-center font-bold  tracking-wide">
           I am a Frontend Developer who enjoys solving interesting problems and
           creating websites people love to use.
         </p>
       </div>
-      <p className="mt-4 introSubText tracking-wide">
+      <p className="mt-4 w-3/4 md:text-base text-sm tracking-wide self-center">
         I am self-taught, currently focusing on the Frontend using primarily
         React, <br />
         but I am always open to learning new frameworks and languages.
