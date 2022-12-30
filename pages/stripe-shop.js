f8b97d02bd24dd86f1e312f62b90aecdf0be7b64
Brandon Rainey <brandonhrainey@gmail.com>
2022-12-30 01:12:49 -0500

    gradient change

--- pages/stripe-shop.js
+++ pages/stripe-shop.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function StripeShop() {
   return (
-    <div>
-        <div>
-            images
+    <div className='flex flex-col items-center'>
+        <div className='grid grid-cols-2 lg:grid-cols-3 w-2/3 gap-2'>
+            <img src='/stripeShopSS2.png' className='border-2 border-red-600 w-auto  lg:col-span-3 object-contain'></img>
+            <img src='/stripeShopSS2.png' className='border-2 border-red-600 w-auto  object-contain'></img>
+            <img src='/stripeShopSS2.png' className='border-2 border-red-600 w-auto  object-contain'></img>
+            <img src='/stripeShopSS2.png' className='border-2 border-red-600 w-auto  object-contain'></img>
         </div>
         <div>
             Description/technology
